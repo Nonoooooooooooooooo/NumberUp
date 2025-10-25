6a29490f0d2deae9df3ae671264f05654d173596
Nonoooooooooooooooo <leseigneurnolann@outlook.fr>
2025-10-25 15:34:36 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+import './globals.css'; // ← ajoute cette ligne en haut
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
       <body>{children}</body>
     </html>
-  );
+  )
 }
